6d22f32bf2ac664f18a1736fe1a9c6392868acf8
LinlyBoi <libkyy@e.email>
2023-03-28 10:19:10 +0200

    digger function for slicing columns (swoosh)

--- py_scripts/digger.py
+++ py_scripts/digger.py
@@ -2,59 +2,42 @@
 import pandas as pd
 import numpy as np
 
-#reading the data
+# reading the data
 # -> MAKE SURE OF THE DATA FRAMES NAMES PEFORE YOU RUN IT
 
 
+df1 = pd.read_csv("output_6th_df.csv")
+df2 = pd.read_csv("vgsales-12-4-2019-short.csv")
 
 
-df1= pd.read_csv('output_6th_df.csv')
-df2= pd.read_csv('vgsales-12-4-2019-short.csv')
+# ----------------------------------------------------------
 
 
-#----------------------------------------------------------
+# print(pf1.head)
+# print(pf2.head)
 
-
-#print(pf1.head)
-#print(pf2.head)
-
-#---------------------------------------------------------
+# ---------------------------------------------------------
 
 
 #                    merging
 
-df_compine= df1.merge(df2, left_on='Name',right_on='Name',how='left')
+df_compine = df1.merge(df2, left_on="Name", right_on="Name", how="left")
 print(df_compine)
-df_compine.to_csv('output_final_df.csv')
+df_compine.to_csv("output_final_df.csv")
 df = df_compine
-output_df = 'N There U Have It'
+output_df = "N There U Have It"
 
-#---------------------------------------------------------
-
+# ---------------------------------------------------------
 
 
 #                          CLEANING -> REMOVING THE WORD REVIOW AND ANY THING AFTER IT
+def slice_column(input_df, output_df, column, expression=" "):
+    unclean = input_df[column].to_list()
+    clean = list()
+    for record in unclean:
+        record = record.split(expression)[0]
+        clean.append(record)
 
-
-
-
-# nuke=df1['GameName'].to_list()
-# nuke2 = list()
-
-# for orphan in nuke : 
-#     orphan = orphan.split('Review')[0]
-#     nuke2.append(orphan)
-
-# df1['GameName']=nuke
-
-
-# print(df1)
-
-# nuke_frame = pd.DataFrame(nuke2)
-# df1=df1.drop(columns=['GameName'])
-
-# df1['Name'] = nuke2
-
-# df1.to_csv('output_6th_df.csv')
-# print(df1)
-
+    input_df = input_df.drop(columns=[column])
+    input_df[column] = clean
+    input_df.to_csv(output_df)
