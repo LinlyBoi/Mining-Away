4b30cc55b17c4a46434234cbd631de644263b02e
LinlyBoi <libkyy@e.email>
2023-03-29 22:22:29 +0200

    I am a scout and I am scouting a hole

--- py_scripts/scout.py
+++ py_scripts/scout.py
@@ -1,1 +1,11 @@
 # Regression/Prediction (Totally gonna do later trust bro)
+
+from sklearn.impute import SimpleImputer
+import numpy as np
+
+
+def cure_depression(dataset, column):
+    dataset[column].fillna(dataset[column].mean(), inplace=True)
+    print("what you want")
+    print(dataset)
+    return dataset[column].to_list()
